7a75bbc0e8d9c1f4f1f1bbbf1482fe1eab1e4e95
Timothy Oei <timothyoei.dev@gmail.com>
2024-03-11 22:06:40 +0000

    Added inference code

---
+++ src/server/model/download_model.py
@@ -0,0 +1,29 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+import os
+from dotenv import load_dotenv
+
+def download_model(model_id, model_path, access_token):
+  tokenizer = AutoTokenizer.from_pretrained(model_id, token=access_token)
+  model = AutoModelForCausalLM.from_pretrained(model_id, token=access_token)
+
+  tokenizer.save_pretrained(model_path)
+  model.save_pretrained(model_path)
+
+def test_model(query, model_path):
+  tokenizer = AutoTokenizer.from_pretrained(model_path)
+  model = AutoModelForCausalLM.from_pretrained(model_path)
+
+  inputs = tokenizer.encode(query, return_tensors="pt")
+  outputs = model.generate(inputs, num_return_sequences=1, repetition_penalty=1.1)
+  return tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+if __name__ == "__main__":
+  load_dotenv()
+
+  model_id = os.getenv("API_MODEL_ID")
+  model_path = "src/server/model/bin"
+  access_token = os.getenv("HF_TOKEN")
+  download_model(model_id, model_path, access_token)
+
+  query = "What color is the sky?"
+  print(test_model(query, model_path))
